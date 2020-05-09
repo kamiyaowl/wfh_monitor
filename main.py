131cab68ee1bfca6287a3684a9fba63e98be9fe8
K <kamiyaowl@gmail.com>
2020-05-09 19:40:11 +0900

    Close #1 : raspiは仰々しすぎる

--- main.py
+++
@@ -1,6 +0,0 @@
-import sys,os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/e-Paper/RaspberryPi&JetsonNano/python/lib/")
-from waveshare_epd import epd2in7b
-
-if __name__ == "__main__":
-    print("hello")
