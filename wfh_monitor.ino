d270b13c21b580346fd1442f762e73c2484dd518
K <kamiyaowl@gmail.com>
2020-05-24 18:46:03 +0900

    close #4 bme680も動いた

--- wfh_monitor.ino
+++ wfh_monitor.ino
@@ -2,18 +2,15 @@
 #include <Wire.h>
 
 static TwoWire &wireL = Wire;  // left  port
-static TwoWire &wireR = Wire1; // right  port
 
 #include <LovyanGFX.hpp>
 #include <Digital_Light_TSL2561.h>
-
-#define SEEED_BM680_WIRE (wireR)
 #include <seeed_bme680.h>
 
 static LGFX lcd;               
 static LGFX_Sprite sprite(&lcd);
 static TSL2561_CalculateLux &lightSensor = TSL2561; // TSL2561 Digital Light Sensor
-static Seeed_BME680 bme680(0x76);                   // BME680 SlaveAddr=0x76
+static Seeed_BME680 bme680((uint8_t)0x76);                   // BME680 SlaveAddr=0x76
 
 void setup() {
     // for POR
@@ -22,7 +19,6 @@ void setup() {
     // setup peripheral 
     Serial.begin(9600);
     wireL.begin();
-    wireR.begin();
 
     // setup modules
     lcd.init();
@@ -32,16 +28,32 @@ void setup() {
 
 static uint32_t loopCounter = 0;
 void loop() {
+    bme680.read_sensor_data();
+    const float   tempature  = bme680.sensor_result_value.temperature;
+    const float   pressure   = bme680.sensor_result_value.pressure / 1000.0f;
+    const float   humidity   = bme680.sensor_result_value.humidity;  
+    const float   gas        = bme680.sensor_result_value.gas / 1000.0f;
     const int32_t visibleLux = lightSensor.readVisibleLux();
 
     Serial.print(visibleLux);
-    Serial.println(",");
+    Serial.print(",");
+    Serial.print(tempature);
+    Serial.print(",");
+    Serial.print(pressure);
+    Serial.print(",");
+    Serial.print(humidity);
+    Serial.print(",");
+    Serial.print(gas);
+    Serial.print(",");
+    Serial.println("");
 
     lcd.setCursor(0, 0);
-    lcd.printf("counter=%d", loopCounter);
-    lcd.println("");
-    lcd.printf("visibleLux=%03d", visibleLux);
-    lcd.println("");
+    lcd.printf("counter=%d\n", loopCounter);
+    lcd.printf("visibleLux=%03d\n", visibleLux);
+    lcd.printf("tempature=%f\n", tempature);
+    lcd.printf("pressure=%f\n", pressure);
+    lcd.printf("humidity=%f\n", humidity);
+    lcd.printf("gas=%f\n", gas);
 
     loopCounter++;
 }
